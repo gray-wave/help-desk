cbe9dcfe46008c257688f9fd7b9955a65e1a18f1
Gray Wave <minikow6969@gmail.com>
2025-07-18 11:27:43 -0400

    Create HelloWorld.java

---
+++ chapter-1-foundations/src/HelloWorld.java
@@ -0,0 +1,40 @@
+/* Here, we have the most simple Java program.
+|  Java may look intimidating at first, but 
+|  it's just rather time-consuming once you
+|  get the hang of it.
+|  
+|  By convention, our class should have the
+|  same name as the filename.
+|  (See Line # 35) ----
+|
+|  This here that you are reading is what is 
+|  known as a multi-line comment. It is prefaced
+|  by a forward slash and an asterisk and
+|  closed by an asterisk followed by a 
+|  forward slash. The pipes to the left are
+|  unnecessary, but I like to put them in
+|  for a more easily readable format scheme.
+|  (See Line # 1 and Line # 33) ----
+|
+|  Each Java program that executes must have
+|  a "main" function, using the main keyword.
+|  This is the "entry point" of our program.
+|  (See Line # 36) ----
+|
+|  After the main function, we have our
+|  procedural logic. Note that each line
+|  within our function must end with a
+|  semi-colon in order to compile properly.
+|  (See Line # 37) ----
+|
+|  I suggest taking the time to write this out
+|  on pen and paper until you can regurgitate
+|  this syntax from memory.
+*/
+
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
+}
+// Also, note that two forward slashes represent a single line and/or "in-line" comment.  
