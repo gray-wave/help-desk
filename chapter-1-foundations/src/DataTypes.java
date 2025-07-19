ad6035340734cda251d1908d9d4093310b5bf6e5
Gray Wave <minikow6969@gmail.com>
2025-07-18 21:55:23 -0400

    Create DataTypes.java

---
+++ chapter-1-foundations/src/DataTypes.java
@@ -0,0 +1,21 @@
+/*
+|  Here, we will talk about some very basic
+|  data types that should assist you with the
+|  week one homework.
+|  
+|  >>> UNDER CONSTRUCTION <<<
+|  Note; need to do printf first.
+|
+|
+*/
+public class DataTypes{
+  public static void main(String[] args){
+    int x;                // Variable declaration (int)
+    x = 5;                // Variable assignment  (assigning value 5 to x)
+    int y = 10;           // One line declare/assign
+    float avg = (y + x)/2 // You can also do inline math
+    
+    String message;   // Variable declaration (String)
+    message = "The sum of these numbers is: ";               // Variable assignment
+    String message2 = "The average of these numbers is: ";   // One line declare/assign
+  
