0ac59ac05d0469fad5c89b3bd32ab4673ff0967e
Gray Wave <minikow6969@gmail.com>
2025-07-20 09:29:56 -0400

    Create FormatPrint.java

---
+++ chapter-1-foundations/src/FormatPrint.java
@@ -0,0 +1,14 @@
+/*
+|  >>>   UNDER CONSTRUCTION   <<<
+|
+|
+|
+|
+*/
+public class FormatPrint{
+  public static void main(String[] args){
+    String name = "Gray Wave";
+    float grade = 72.8735;
+    float hourlyWage = 12.45;
+    System.out.printf("%s%s%.2f%n", "Hello, my name is ",name);
+    System.out.printf("%s%s%.2f%n", "My hourly wage is ",hourlyWage);
