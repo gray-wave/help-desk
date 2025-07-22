b1714b8b1d7d7c9fda50b473d80625bc5634ea7d
Gray Wave <minikow6969@gmail.com>
2025-07-22 17:01:38 -0400

    Update FormatPrint.java

--- chapter-1-foundations/src/FormatPrint.java
+++ chapter-1-foundations/src/FormatPrint.java
@@ -1,5 +1,5 @@
 /*
-|  In java, there are 3 types of System.out
+|  In Java, there are 3 types of System.out
 |  printing things that we can do. Each different
 |  print method has its benefits and drawbacks.
 |
@@ -12,17 +12,25 @@
 |  can print multiple datatypes; however, it does
 |  not append the newline character to the end of
 |  arguments specified.
-|  >>>     UNDER CONSTRUCTION     <<<
 |
+|  Last, but certainly not least, is System.out.printf().
+|  printf allows for the usage of "format specifiers,"
+|  which allow the programmer to hard-code which datatype
+|  arguments are to be specified, and in which order they
+|  appear. 
+|  
+|  UNDER CONSTRUCTION (very close to being done!!)
 */
 public class FormatPrint {
   public static void main(String[] args){
+    // Variable declaration and assignment
     String name = "Gray Wave";
     double grade = 72.8735;
     double hourlyWage = 12.45;
-    System.out.print("Hello world! ");
-    System.out.printf("%s%s%n", "Hello, my name is ",name); 
-    System.out.printf("%s%.2f%n", "My hourly wage is ",hourlyWage);
-    System.out.println(); //brb
+    // End variable declaration and assignment
+    System.out.print("Hello world! ");                              //print example
+    System.out.printf("%s%s%n", "Greetings, my name is ",name);     //printf example with just strings
+    System.out.printf("%s%.2f%n", "My hourly wage is ",hourlyWage); //printf example with decimal numbers and strings.
+    System.out.println(grade + "% is my current grade.");           //println example
   }
 }
