f344cae1d4206192d9b26814994331e497d21806
Gray Wave <minikow6969@gmail.com>
2025-07-22 17:48:55 -0400

    Update DataTypes.java

--- chapter-1-foundations/src/DataTypes.java
+++ chapter-1-foundations/src/DataTypes.java
@@ -12,15 +12,16 @@
 */
 public class DataTypes{
   public static void main(String[] args){
-    int x;                // Variable declaration (int)
-    x = 5;                // Variable assignment  (assigning value 5 to x)
-    int y = 10;           // One line declare/assign
-    float avg = (y + x)/2 // You can also do inline math
+    int x;                  // Variable declaration (int)
+    x = 5;                  // Variable assignment  (assigning value 5 to x)
+    int y = 10;             // One line declare/assign
+    float avg = (y + x)/2   // You can also do inline math
     
-    String message;   // Variable declaration (String)
+    String message;         //  <---        <-----    // Variable declaration (String)
     message = "The sum of these numbers is: ";               // Variable assignment
     String message2 = "The average of these numbers is: ";   // One line declare/assign
-    System.out.printf("%s%.2f%n", message2, avg);
+    System.out.println(message, (x + y));                    // Doing in-line math with println
+    System.out.printf("%s%.2f%n", message2, avg);            // Using print formatting
   }
 }
 // I keep forgetting my closing bracers lol
