ca36da3874720e4dafd13a11e2134d0c5e25acd9
Gray Wave <minikow6969@gmail.com>
2025-07-22 16:52:33 -0400

    Update FormatPrint.java

--- chapter-1-foundations/src/FormatPrint.java
+++ chapter-1-foundations/src/FormatPrint.java
@@ -1,17 +1,28 @@
 /*
-|  >>>   UNDER CONSTRUCTION   <<<
+|  In java, there are 3 types of System.out
+|  printing things that we can do. Each different
+|  print method has its benefits and drawbacks.
 |
+|  The most commonly used output method is 
+|  System.out.println(), which can print multiple
+|  datatypes and appends a newline character to
+|  the end of whatever arguments specified.
 |
-|
+|  Next, there is System.out.print(), which also
+|  can print multiple datatypes; however, it does
+|  not append the newline character to the end of
+|  arguments specified.
+|  >>>     UNDER CONSTRUCTION     <<<
 |
 */
-public class FormatPrint{
+public class FormatPrint {
   public static void main(String[] args){
     String name = "Gray Wave";
-    float grade = 72.8735;
-    float hourlyWage = 12.45;
-    System.out.printf("%s%s%.2f%n", "Hello, my name is ",name); 
-    System.out.printf("%s%s%.2f%n", "My hourly wage is ",hourlyWage);
-    System.out.printf("%d%s%n", grade, " %, is my unrounded grade");
+    double grade = 72.8735;
+    double hourlyWage = 12.45;
+    System.out.print("Hello world! ");
+    System.out.printf("%s%s%n", "Hello, my name is ",name); 
+    System.out.printf("%s%.2f%n", "My hourly wage is ",hourlyWage);
+    System.out.println(); //brb
   }
 }
