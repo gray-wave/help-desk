a9f4ba8fc61ab076bd7a1dfd0fa3438a2eceb76b
Gray Wave <minikow6969@gmail.com>
2025-07-22 14:28:14 -0400

    Update FormatPrint.java

--- chapter-1-foundations/src/FormatPrint.java
+++ chapter-1-foundations/src/FormatPrint.java
@@ -10,5 +10,8 @@ public class FormatPrint{
     String name = "Gray Wave";
     float grade = 72.8735;
     float hourlyWage = 12.45;
-    System.out.printf("%s%s%.2f%n", "Hello, my name is ",name);
+    System.out.printf("%s%s%.2f%n", "Hello, my name is ",name); 
     System.out.printf("%s%s%.2f%n", "My hourly wage is ",hourlyWage);
+    System.out.printf("%d%s%n", grade, " %, is my unrounded grade");
+  }
+}
