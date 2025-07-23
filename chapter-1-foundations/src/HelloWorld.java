96c7495a897f929aeb4f9d558fba9d93a5005efa
Gray Wave <minikow6969@gmail.com>
2025-07-23 18:30:11 -0400

    Update HelloWorld.java

--- chapter-1-foundations/src/HelloWorld.java
+++ chapter-1-foundations/src/HelloWorld.java
@@ -1,11 +1,12 @@
-/* Here, we have the most simple Java program.
+/* 
+|  Here, we have the most simple Java program.
 |  Java may look intimidating at first, but 
 |  it's just rather time-consuming once you
 |  get the hang of it.
 |  
 |  By convention, our class should have the
 |  same name as the filename.
-|  (See Line # 35) ----
+|  (See Line # 36) ----
 |
 |  This here that you are reading is what is 
 |  known as a multi-line comment. It is prefaced
@@ -14,18 +15,18 @@
 |  forward slash. The pipes to the left are
 |  unnecessary, but I like to put them in
 |  for a more easily readable format scheme.
-|  (See Line # 1 and Line # 33) ----
+|  (See Line # 1 and Line # 34) ----
 |
 |  Each Java program that executes must have
 |  a "main" function, using the main keyword.
 |  This is the "entry point" of our program.
-|  (See Line # 36) ----
+|  (See Line # 37) ----
 |
 |  After the main function, we have our
 |  procedural logic. Note that each line
 |  within our function must end with a
 |  semi-colon in order to compile properly.
-|  (See Line # 37) ----
+|  (See Line # 38) ----
 |
 |  I suggest taking the time to write this out
 |  on pen and paper until you can regurgitate
