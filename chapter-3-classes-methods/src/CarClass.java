b74baafb14c298eb69cdf386347cd7e0f30200d3
Gray Wave <minikow6969@gmail.com>
2025-07-30 14:40:46 -0400

    Update CarClass.java

--- chapter-3-classes-methods/src/CarClass.java
+++ chapter-3-classes-methods/src/CarClass.java
@@ -7,16 +7,21 @@ public class CarClass{    // Class declaration
   private String make;    // instance variable
   private String model;   // instance variable
 // Set Methods
-public void setMake(){
-  this.make = make;
+public void setMake(String make)    // Function parameter, accepts a string argument
+{
+  this.make = make;                 // Sets the argument/prior parameter
 }
-public void setModel(){
+public void setModel(String model)  // Function parameter, accepts a string argument
+{
   this.model = model;
 }
-// Get Methods
-public String getMake(){
+// End Set Methods, Begin Get Methods
+public String getMake()
+{
   return make;
 }
-public String getModel(){
+public String getModel()
+{
   return model;
 }
+// End Get Methods
