5ae8679a7e6ff651163c7e83d1effc6ad3859056
Gray Wave <minikow6969@gmail.com>
2025-07-30 13:09:54 -0400

    Create SimpleDecision.java

---
+++ chapter-2-io/src/SimpleDecision.java
@@ -0,0 +1,23 @@
+/*
+|
+|  >>>   UNDER CONSTRUCTION   <<<
+|
+*/
+import java.util.Scanner;
+public class SimpleDecision{
+  public static void main(String[] args){
+    Scanner in = new Scanner(Sytem.in);
+    System.out.print("Enter two integer numbers: ");
+    int num1 = in.nextInt();
+    int num2 = in.nextInt();
+    if (num1 == num2) {
+      System.out.printf("%d%s%d", num1, " is the same as ", num2);
+        }
+    if (num1 < num2) {
+      System.out.printf("%d%s%d", num1, " is less than ", num2);
+    }
+    if (num1 > num2) {
+      System.out.printf("%d%s%d", num1, " is greater than", num2);
+    }
+  }
+} 
