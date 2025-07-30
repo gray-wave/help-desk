96339d4c38fbc6b9c8aadfaa97a9cd7cc5b6d21e
Gray Wave <minikow6969@gmail.com>
2025-07-30 14:11:33 -0400

    Create CarClass.java

---
+++ chapter-3-classes-methods/src/CarClass.java
@@ -0,0 +1,22 @@
+/*
+|
+|   >>>   UNDER CONSTRUCTION   <<<
+|
+*/
+public class CarClass{    // Class declaration
+  private String make;    // instance variable
+  private String model;   // instance variable
+// Set Methods
+public void setMake(){
+  this.make = make;
+}
+public void setModel(){
+  this.model = model;
+}
+// Get Methods
+public String getMake(){
+  return make;
+}
+public String getModel(){
+  return model;
+}
