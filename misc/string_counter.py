ab559b0d0ec8cc9b4d2785e55fe4fdbee34e11c1
Gray Wave <minikow6969@gmail.com>
2025-09-16 10:36:25 -0400

    Create string_counter.py

---
+++ misc/string_counter.py
@@ -0,0 +1,14 @@
+# char counter
+
+def counter(a: list) -> int:
+    return int(len(a))
+
+def loop():
+    dummy = int(input("Enter positive integer to continue\t"))
+    while dummy > 0:
+        string_ct = str(input("Enter a string to count:  "))
+        ctd_string = counter(string_ct)
+        print(ctd_string)
+        dummy = int(input("Enter positive integer to continue\t"))
+
+loop()
